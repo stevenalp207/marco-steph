c368fe2a85720e94d878aba37fef2396570c6383
stevenalp207 <s.alpizar207@gmail.com>
2026-05-06 19:36:42 -0600

    feat: add additional guest reservations to the reservations data

--- src/data/reservations.js
+++ src/data/reservations.js
@@ -27,6 +27,84 @@ export const guestReservations = [
   { reservation: 'Cascante', name: 'Marco Cascante', passes: 1 },
   { reservation: 'Castro', name: 'Francisco Castro', passes: 1 },
   { reservation: 'Leon', name: 'Daniel Leon', passes: 1 },
+
+  { reservation: 'Mora', name: 'Familia Mora (Marco Mora, Carmen Gonzalez, Margartia Mora, Maria del Carmen Mora, Santiago Mora, Emilio Mora, Mama Mary)', passes: 7 },
+
+  { reservation: 'Jorge Gonzalez', name: 'Jorge Gonzalez', passes: 2 },
+
+  { reservation: 'Antonio Gonzalez', name: 'Antonio Gonzalez', passes: 4 },
+
+  { reservation: 'Tio Cocoro', name: 'Tio Cocoro', passes: 5 },
+
+  { reservation: 'Tio Memo', name: 'Tio Memo', passes: 6 },
+
+  { reservation: 'Tia Susy', name: 'Tia Susy', passes: 5 },
+
+  { reservation: 'Tia Car', name: 'Tia Car', passes: 2 },
+
+  { reservation: 'Tia Tere', name: 'Tia Tere', passes: 8 },
+
+  { reservation: 'Tia Piri', name: 'Tia Piri', passes: 4 },
+
+  { reservation: 'Tia Vero', name: 'Tia Vero', passes: 6 },
+
+  { reservation: 'Tio Colay', name: 'Tio Colay', passes: 5 },
+
+  { reservation: 'Tia Elena', name: 'Tia Elena', passes: 7 },
+
+  { reservation: 'Tia Angelina', name: 'Tia Angelina', passes: 1 },
+
+  { reservation: 'Tia Pilar', name: 'Tia Pilar', passes: 1 },
+
+  { reservation: 'Cynthia Fierro', name: 'Cynthia Fierro', passes: 2 },
+
+  { reservation: 'Luis Vega', name: 'Luis Vega', passes: 1 },
+
+  { reservation: 'Mario Sanchez', name: 'Mario Sanchez', passes: 1 },
+
+  { reservation: 'Maricela Reyes', name: 'Maricela Reyes', passes: 1 },
+
+  { reservation: 'Clara', name: 'Clara', passes: 2 },
+
+  { reservation: 'Hugo', name: 'Hugo', passes: 1 },
+
+  { reservation: 'Carina Brindis', name: 'Carina Brindis', passes: 2 },
+
+  { reservation: 'Diego Serafin', name: 'Diego Serafin', passes: 2 },
+
+  { reservation: 'Pachecho', name: 'Pachecho', passes: 2 },
+
+  { reservation: 'Hernan', name: 'Hernan', passes: 1 },
+
+  { reservation: 'Minerva', name: 'Minerva', passes: 2 },
+
+  { reservation: 'Jorge Hurtado', name: 'Jorge Hurtado', passes: 2 },
+
+  { reservation: 'Sergio Anzaldo', name: 'Sergio Anzaldo', passes: 2 },
+
+  { reservation: 'Ingrid Zuleta', name: 'Ingrid Zuleta', passes: 2 },
+
+  { reservation: 'Bayron Duran', name: 'Bayron Duran', passes: 2 },
+
+  { reservation: 'Fernando Sosa', name: 'Fernando Sosa', passes: 2 },
+
+  { reservation: 'Paco Perez', name: 'Paco Perez', passes: 2 },
+
+  { reservation: 'Oliver Rodriguez', name: 'Oliver Rodriguez', passes: 2 },
+
+  { reservation: 'Braulio', name: 'Braulio', passes: 2 },
+
+  { reservation: 'Ericka', name: 'Ericka', passes: 2 },
+
+  { reservation: 'Alejandro Lopez', name: 'Alejandro Lopez', passes: 2 },
+
+  { reservation: 'Lourdes Duran', name: 'Lourdes Duran', passes: 1 },
+
+  { reservation: 'Jorge Espinoza', name: 'Jorge Espinoza', passes: 2 },
+
+  { reservation: 'Yatzira Camacho', name: 'Yatzira Camacho', passes: 2 },
+
+  { reservation: 'Gaby Dominguez', name: 'Gaby Dominguez', passes: 2 },
 ]
 
 export function normalizeText(value = '') {
